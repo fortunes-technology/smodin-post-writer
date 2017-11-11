56ea599c78ad5ee913a822344c1091d8a0eb742d
KevinDanikowski <kdanikowski@outlook.com>
2017-11-10 22:57:28 -0600

    with '#' will be ignored, and an empty message aborts the commit.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
